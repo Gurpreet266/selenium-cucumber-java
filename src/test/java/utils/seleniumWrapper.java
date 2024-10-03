fc0b26c12b18c3af7dfe1a22a359e84aa12604ed
Gurpreet266 <simrankaurmaan.sk@gmail.com>
2024-10-03 22:04:27 +0530

    use of wrapper

---
+++ src/test/java/utils/seleniumWrapper.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class seleniumWrapper {
+
+    WebDriver driver = new EdgeDriver();
+
+       public boolean clickElement(By locator){
+        driver.findElement(locator).click();
+        return true;
+
+    }
+
+    //function 2 - isDisplayed
+    public boolean displayElement(By locator){
+        driver.findElement(locator).isDisplayed();
+        return true;
+    }
+
+    //function 3 - quit browser
+    public boolean quitElement(By locator){
+        driver.quit();
+        return true;
+    }
+
+
+    //function 4 - close current tab
+    public boolean closeElement(By locator) {
+        driver.close();
+        return true;
+    }
+}
