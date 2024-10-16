7394a4e937c9eb51ed0adce78cc4883cc21238c3
Gurpreet266 <simrankaurmaan.sk@gmail.com>
2024-10-16 20:45:51 +0530

    changes in functions name

--- src/test/java/utils/seleniumWrapper.java
+++ src/test/java/utils/seleniumWrapper.java
@@ -9,7 +9,7 @@ public class seleniumWrapper {
 
     seleniumWrapper wrapper = new seleniumWrapper();
 
-    WebDriver driver = new EdgeDriver();
+public WebDriver driver = new EdgeDriver();
 
        public boolean clickElement(By locator){
         driver.findElement(locator).click();
@@ -18,20 +18,20 @@ public class seleniumWrapper {
     }
 
     //function 2 - isDisplayed
-    public boolean displayElement(By locator){
+    public boolean checkElementDisplay(By locator){
        boolean flag=driver.findElement(locator).isDisplayed();
         return flag;
     }
 
     //function 3 - quit browser
-    public boolean quitElement(){
+    public boolean quitBrowser(){
         driver.quit();
         return true;
     }
 
 
     //function 4 - close current tab
-    public boolean closeElement() {
+    public boolean closeCurrentBrowserWindow() {
         driver.close();
         return true;
     }
