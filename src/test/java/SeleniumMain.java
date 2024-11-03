b903f72ea7b46f5576720c8f84cfbd05c1381d61
Gurpreet266 <simrankaurmaan.sk@gmail.com>
2024-11-03 17:46:43 +0530

    waitForWindowCount function Added.

--- src/test/java/SeleniumMain.java
+++ src/test/java/SeleniumMain.java
@@ -2,45 +2,33 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import utils.seleniumWrapper;
+import utils.SeleniumWrapper;
 
-import java.time.Duration;
 
 public class SeleniumMain {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        SeleniumWrapper seleniumWrapper = new SeleniumWrapper();
 
         //driver.get("https://www.geeksforgeeks.org/");
-
-//         By pageOneLinkLocator = By.linkText("Page One");
-//         driver.findElement(pageOneLinkLocator).click();
-//        By omyIdLocator = By.id("header-inner");
-//        driver.findElement(omyIdLocator).click();
-//        System.out.println(driver.getTitle());
-//        System.out.println(driver.getPageSource());
-//        System.out.println(driver.getCurrentUrl());
-//        System.out.println(driver.getClass());
-//        System.out.println(driver.getWindowHandles());
-//        System.out.println(driver.getWindowHandle());
-//        System.out.println(driver.navigate());
-//        System.out.println(driver.manage());
-//        System.out.println(driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)));
-//        driver.quit();
-        // Test 1: verify pageOne new page is opened when user clicks on pageOne link
-        // open browser
-        System.setProperty("webdriver.edge.driver", "C:\\Users\\GURPREET KAUR\\Downloads\\MyFirstProject\\src\\test\\resources\\drivers\\msedgedriver.exe");
+        //PAN reg number do not delete, do not commit 0165969288, ack no. 881151100765600
+        System.setProperty("web driver.edge.driver", "C:\\Users\\GURPREET KAUR\\Downloads\\MyFirstProject\\src\\test\\resources\\drivers\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://omayo.blogspot.com/");
         boolean flag = driver.findElement(By.id("selenium143")).isDisplayed();
-        Assert.assertTrue("Home page is not opened",flag);
+        Assert.assertTrue("Home page is not opened", flag);
         driver.findElement(By.id("selenium143")).click();
+
+
+        seleniumWrapper.waitForNumberOfOpenWindowsToBe(2,10);
 
 
         //Check url of new tab is correct to validate the new tab is opened
         //Step 1 - check 2 tabs are open
-        wrapper.windowsCount();
+        int windowsCount = seleniumWrapper.getWindowsCount();
+        Assert.assertEquals("Second window not opened", 2, windowsCount);// check /verify/validate means assertion lani
 
         //Step 2 - Switch to last opened tab (ie 2nd in our case)
         driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
@@ -52,49 +40,11 @@ public class SeleniumMain {
         //Step 4 - Assert the url got in step 3 matches the expected url
         String actualUrl = driver.getCurrentUrl();
         String expectedUrl = "https://selenium143.blogspot.com/";
-       // Assert.isTrue(flag,"new page not opened");
-        Assert.assertEquals("new page not opened",expectedUrl, actualUrl);
+        // Assert.isTrue(flag,"new page not opened");
+        Assert.assertEquals("new page not opened", expectedUrl, actualUrl);
         driver.quit();
-        seleniumWrapper obj = new seleniumWrapper(); // package name.class name "obj" is the object we created to call functions
-        obj.closeElement();                                      // of the class "seleniumWrapper"
-
-//        // open website
-//        driver.get("https://omayo.blogspot.com/");
-//
-//        // verify home page is opened                   (we will find a unique element i.e. "Page One" as in this e.g.)
-//        boolean flag =  driver.findElement(By.linkText("Page One")).isDisplayed();
-//        Assert.isTrue(flag,"Home page is not opened");
-//
-//
-//        // click on pageOne link
-//        driver.findElement(By.linkText("Page One")).click();
-//
-//        // verify new pageOne page is opened             (for this check the actualUrl)
-//        String actualUrl = driver.getCurrentUrl();
-//        String expectedUrl = "https://omayo.blogspot.com/2013/05/page-one.html";
-//
-//        flag = actualUrl.equals(expectedUrl);
-//        Assert.isTrue(flag,"new page not opened");
-//
-//        driver.quit();
+        // package name.class name "obj" is the object we created to call functions
+        boolean isBrowserClosed = seleniumWrapper.closeCurrentBrowserWindow();
+        Assert.assertTrue("browser not closed", isBrowserClosed);
     }
 }
-
-
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
-
-//public class Geeksforgeeks {
-//   public static void main(String args[])
-//{
-//      System.setProperty("webdriver.edge.driver", "C:\Users\GURPREET KAUR\Downloads\MyFirstProject\src\test\java\drivers\\msedgedriver.exe");
-// Instantiate a WebDriver class.
-//   WebDriver driver = new EdgeDriver();
-//      Maximize the browser
-//  driver.manage().window().maximize();
-//  Launch Website
-//driver.get("https://www.geeksforgeeks.org/");
-//   System.out.println(driver.findElement(By.id()));
-//    }
-//}  driver.get vs driver.navigate.to()
-
